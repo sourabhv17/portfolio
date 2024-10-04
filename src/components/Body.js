1edd8134be8d22e5d2f38316e57b3c4bf4950b7d
sourabh <sourabh.umarvaishya@gmail.com>
2024-10-04 18:40:04 +0530

    body component complete

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,6 @@
 import "./Body.css";
 import { LinkedIn, Github, Twitter } from "../utils/links";
+import Contacts from "./Contacts";
 
 const Body = () => {
   return (
@@ -16,8 +17,26 @@ const Body = () => {
         </span>
       </div>
       <div className="body-center">
-        <span className="body-name">I'm Sourabh</span>
-        <span>I love coding</span>
+        <div className="body-center-up">
+          <span className="body-name">I'm Sourabh</span>
+          <span>I love coding</span>
+        </div>
+        <div className="body-center-down">
+          <span>
+            My objective is to be the leading player in the field of Technology.
+            I want to apply my expertise in solving problems, innovation in a
+            challenging environment and contribute to the overall growth and
+            success of the organization I join. My goal is to become an
+            outstanding professional with an extensive technical knowledge
+            coupled with strong analytical and communication skills. Being
+            focused, sincere and dedicated, will enable me to achieve my career
+            objectives and set benchmarks for myself.
+          </span>
+          <div className="buttons-div">
+            <button className="btn1">My Portfolio</button>
+            <button class="btn2">Contact Me</button>
+          </div>
+        </div>
       </div>
       <div className="body-right">
         <img src={require("../utils/image.png")}></img>
---
+++ src/components/Contacts.js
@@ -0,0 +1,13 @@
+const Contacts = () => {
+  return (
+    <div>
+      <h3>Name: Sourabh Umarvaishya</h3>
+      <h3>Contacts: 7665504207</h3>
+      <h3>Email: sourabh.umarvaishya@gmail.com</h3>
+      <h3>LinkedIn</h3>
+      <h3>Github</h3>
+    </div>
+  );
+};
+
+export default Contacts;
